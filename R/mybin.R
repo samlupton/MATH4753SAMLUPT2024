b19cd415909f7e4ef539d7245c16e983a9f3a4eb
moo20222 <samuel@ou.edu>
2024-09-23 13:18:29 -0500

    update mybin

--- R/mybin.R
+++ R/mybin.R
@@ -1,9 +1,12 @@
 #' My Bin
 #'
+#' @param x Number of successes
 #' @return a vector of probability
 #' @export
+#' @importFrom stats dbinom
 #'
 #' @examples
-mybin <- function() {
-  dbinom(x,10,0.5)
+#' mybin(5)  # Example usage
+mybin <- function(x) {
+  dbinom(x, size = 10, prob = 0.5)
 }
