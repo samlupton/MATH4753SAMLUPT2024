03502d26a9ba91b13f3f1c98a87ad5b6e5ca37a3
moo20222 <samuel@ou.edu>
2024-10-11 16:21:22 -0500

    data packaged

---
+++ R/fire.R
@@ -0,0 +1,14 @@
+#' Fire damage data set
+#'
+#' A set of data for damage caused by fire
+#' Report ...
+#'
+#' @format ## `fire`
+#' A data frame with 15 rows and 2 columns:
+#' \describe{
+#'   \item{distance}{fire source distance}
+#'   \item{damage}{damage from fire source}
+#'   ...
+#' }
+#' @source na
+"fire"
